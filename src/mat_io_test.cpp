e103abaa7f31136eac12f6fa5336712e1dbb6c35
Rotoslider <3dmapmaker@gmail.com>
2021-02-17 10:59:16 -0800

    Update mat_io_test.cpp
    Fixed to work with opencv4

--- src/mat_io_test.cpp
+++ src/mat_io_test.cpp
@@ -8,7 +8,8 @@ backward::SignalHandling sh;
 #include <code_utils/cv_utils/cv_type.hpp>
 #include <code_utils/sys_utils/cvmat_file_io.hpp>
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 #include <time.h>
 
 using namespace std;
@@ -30,14 +31,14 @@ main( )
 
     //    Mat img1 = imread( "/home/gao/ws/devel/lib/camera_model/image_down/IMG_35.png",
     //    CV_LOAD_IMAGE_GRAYSCALE );
-    Mat img1 = imread( "/home/gao/IMG_1.png", CV_LOAD_IMAGE_UNCHANGED );
+    Mat img1 = imread( "/home/lbackpack/IMG_1.png", IMREAD_UNCHANGED );
 
     cv::resize( img1, img, cv::Size( 640, 512 ) );
 
-    sys_utils::io::writeMatrixToBinary( "/home/gao/img_data", img1 );
+    sys_utils::io::writeMatrixToBinary( "/home/lbackpack/img_data", img1 );
 
     cv::Mat img11;
-    sys_utils::io::parseMatrixFromBinary( "/home/gao/img_data", img11 );
+    sys_utils::io::parseMatrixFromBinary( "/home/lbackpack/img_data", img11 );
     showImg( "img_data", img11 );
 
     cv::Mat img2( 2, 2, CV_8UC3 );
@@ -49,7 +50,7 @@ main( )
         img2.at< cv::Vec12b >( 1, 1 )[i] = uchar( 4 );
     }
     std::cout << img2 << "\n\n";
-    sys_utils::io::writeMatrixToBinary( "/home/gao/img_data2", img2 );
+    sys_utils::io::writeMatrixToBinary( "/home/lbackpack/img_data2", img2 );
 
     cv::Mat img3( 2, 2, CV_32FC3 );
     for ( int i = 0; i < 3; ++i )
@@ -60,7 +61,7 @@ main( )
         img3.at< cv::Vec3f >( 1, 1 )[i] = float( 4.4 );
     }
     std::cout << img3 << "\n\n";
-    sys_utils::io::writeMatrixToBinary( "/home/gao/img_data3", img3 );
+    sys_utils::io::writeMatrixToBinary( "/home/lbackpack/img_data3", img3 );
 
     cv::Mat img4( 2, 2, CV_64FC3 );
     for ( int i = 0; i < 3; ++i )
@@ -71,21 +72,21 @@ main( )
         img4.at< cv::Vec3d >( 1, 1 )[i] = float( 4.44 );
     }
     std::cout << img4 << "\n\n";
-    sys_utils::io::writeMatrixToBinary( "/home/gao/img_data4", img4 );
+    sys_utils::io::writeMatrixToBinary( "/home/lbackpack/img_data4", img4 );
 
     std::cout << "===================================="
               << "\n\n";
 
     cv::Mat img22;
-    sys_utils::io::parseMatrixFromBinary( "/home/gao/img_data2", img22 );
+    sys_utils::io::parseMatrixFromBinary( "/home/lbackpack/img_data2", img22 );
     std::cout << img22 << "\n\n";
 
     cv::Mat img33;
-    sys_utils::io::parseMatrixFromBinary( "/home/gao/img_data3", img33 );
+    sys_utils::io::parseMatrixFromBinary( "/home/lbackpack/img_data3", img33 );
     std::cout << img33 << "\n\n";
 
     cv::Mat img44;
-    sys_utils::io::parseMatrixFromBinary( "/home/gao/img_data4", img44 );
+    sys_utils::io::parseMatrixFromBinary( "/home/lbackpack/img_data4", img44 );
     std::cout << img44 << "\n\n";
 
     waitKey( 0 );
